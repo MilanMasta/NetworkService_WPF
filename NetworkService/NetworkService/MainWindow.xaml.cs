b15869a80a42ab0340c688d11c9e45de9e8b5633
Milan Mastilovic <milanmastilovic1@gmail.com>
2021-11-19 17:36:22 +0100

    Full commit

---
+++ NetworkService/NetworkService/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using NetworkService.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkService
+{
+    public partial class MainWindow : Window
+    {
+
+        private string path;
+        private int id;
+        private double value;
+        private bool file;
+        public MainWindow()
+        {
+          
+            path = Environment.CurrentDirectory + @"\LogFile.txt";
+            InitializeComponent();
+            createListener(); //Povezivanje sa serverskom aplikacijom
+        }
+
+        private void createListener()
+        {
+            var tcp = new TcpListener(IPAddress.Any, 25565);
+            tcp.Start();
+
+            var listeningThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    var tcpClient = tcp.AcceptTcpClient();
+                    ThreadPool.QueueUserWorkItem(param =>
+                    {
+                        //Prijem poruke
+                        NetworkStream stream = tcpClient.GetStream();
+                        string incomming;
+                        byte[] bytes = new byte[1024];
+                        int i = stream.Read(bytes, 0, bytes.Length);
+                        //Primljena poruka je sacuvana u incomming stringu
+                        incomming = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                        //Ukoliko je primljena poruka pitanje koliko objekata ima u sistemu -> odgovor
+                        if (incomming.Equals("Need object count"))
+                        {
+                            //Response
+                            /* Umesto sto se ovde salje count.ToString(), potrebno je poslati 
+                             * duzinu liste koja sadrzi sve objekte pod monitoringom, odnosno
+                             * njihov ukupan broj (NE BROJATI OD NULE, VEC POSLATI UKUPAN BROJ)
+                             * */
+                                     int c = ViewModel.NetworkViewViewModel.monitor;
+                            Byte[] data = System.Text.Encoding.ASCII.GetBytes(DataBase.Servers.Count().ToString());
+                            stream.Write(data, 0, data.Length);
+                            file = false;
+                        }
+                        else
+                        {
+                            //U suprotnom, server je poslao promenu stanja nekog objekta u sistemu
+                            Console.WriteLine(incomming); //Na primer: "Objekat_1:272"
+
+                            //################ IMPLEMENTACIJA ####################
+                            // Obraditi poruku kako bi se dobile informacije o izmeni
+                            // Azuriranje potrebnih stvari u aplikaciji
+                            string[] split = incomming.Split('_', ':');
+                            id = Int32.Parse(split[1]);
+                            value = Double.Parse(split[2]);
+                            if (DataBase.Servers.Count() > 0)
+                            {
+                                DataBase.Servers[id].Value = value;
+                                Upis();
+                            }
+                        }
+                    }, null);
+                }
+            });
+
+            listeningThread.IsBackground = true;
+            listeningThread.Start();
+        }
+
+        private void Upis()
+        {
+            if (!file)
+            {
+                StreamWriter writer;
+                using (writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("Date Time:\t" + DateTime.Now.ToString() + "\tObject_" + id + "\tValue:\t" + value);
+                }
+                file = true;
+            }
+            else
+            {
+                StreamWriter writer;
+                using (writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine("Date Time:\t" + DateTime.Now.ToString() + "\tObject_" + id + "\tValue:\t" + value);
+
+                }
+            }
+            file = true;
+        }
+    }
+}
---
+++ NetworkService/NetworkService/ViewModel/DataChartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using NetworkService.Core;
+using NetworkService.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.ViewModel
+{
+    public class DataChartViewModel : BindableBase
+    {
+        public static GraphUpdater ElementHeights { get; set; } = new GraphUpdater();
+
+        public static double CalculateElementHeight(double value)
+        {
+                return value*2;
+        }
+    }
+        
+ }
